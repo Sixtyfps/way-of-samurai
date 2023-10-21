8cd13bae2d42c10dec0b071519e68b5dbda22e94
JK <viletert@gmail.com>
2023-10-21 16:27:26 +0300

    dialogs structure (21)

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -6,8 +6,29 @@ type DialogsType = {
 }
 export const Dialogs: React.FC<DialogsType> = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={`${s.dialogs}` + ' ' + `${s.active}`}>
+                    Dimich
+                </div>
+                <div className={s.dialog}>
+                    Dim
+                </div>
+                <div className={s.dialog}>
+                    Dim
+                </div>
+                <div className={s.dialog}>
+                    Dim
+                </div>
+                <div className={s.dialog}>
+                    Dim
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.dialog}>Hi!</div>
+                <div className={s.dialog}>Wassup</div>
+                <div className={s.dialog}>How you doin?</div>
+            </div>
         </div>
     )
 }
