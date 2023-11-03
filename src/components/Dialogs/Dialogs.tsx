e1371df5d7fbbfdd26b0ecad046862f512a2a3aa
JK <viletert@gmail.com>
2023-11-03 19:27:25 +0200

    refactoring (30)

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -11,22 +11,6 @@ type DialogsType = {
 
 export const Dialogs: React.FC<DialogsType> = (props) => {
 
-    // let dialogs = [
-    //     {id: 1, name: 'Dimich'},
-    //     {id: 2, name: 'Andrey'},
-    //     {id: 3, name: 'John'},
-    //     {id: 4, name: 'Sam'},
-    //     {id: 5, name: 'Artem'},
-    // ]
-    //
-    // let messages = [
-    //     {id: 1, message: 'Hi'},
-    //     {id: 2, message: 'Wassup!'},
-    //     {id: 3, message: 'How are you'},
-    //     {id: 4, message: 'Bye'},
-    //     {id: 5, message: 'Night!'},
-    // ]
-
     let dialogsElements = props.state.dialogs.map((d, key) => <DialogItem name={d.name} id={d.id} key={key}/>)
     let messagesElements = props.state.messages.map((m, key) => <Message message={m.message} key={key}/>)
 
