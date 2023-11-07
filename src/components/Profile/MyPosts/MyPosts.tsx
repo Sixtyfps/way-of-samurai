b2356a6daddcc59560a67fd61f14dd3431fc01ca
JK <viletert@gmail.com>
2023-11-07 20:14:46 +0200

    input value getting method changed from ref to e.currentTarget.value (34)

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {ChangeEvent} from "react";
 import s from './MyPosts.module.css'
 import {Post} from "./Post/Post";
 import {ProfilePageType} from "../../../redux/state";
@@ -15,18 +15,12 @@ export const MyPosts: React.FC<MyPostsType> = (props) => {
     let postsElements = props.state.posts.map((p, key) => <Post message={p.message} likesCount={p.likesCount}
                                                                 key={key}/>)
 
-    let newPostElement = React.createRef<HTMLTextAreaElement>()
-
     const addPost = () => {
-        if (newPostElement.current) {
-            props.addPost(newPostElement.current.value)
-        }
+            props.addPost(props.newPostText)
     }
 
-    const onChangeHandler = () => {
-        if (newPostElement.current) {
-            props.updateNewPostText(newPostElement.current.value)
-        }
+    const onChangeHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
+            props.updateNewPostText(e.currentTarget.value)
     }
 
     return (
@@ -35,7 +29,7 @@ export const MyPosts: React.FC<MyPostsType> = (props) => {
                 <h3>My posts</h3>
                 <div>
                     <div>
-                        <textarea onChange={onChangeHandler} ref={newPostElement} value={props.newPostText}/>
+                        <textarea onChange={onChangeHandler} value={props.newPostText}/>
                     </div>
                     <div>
                         <button onClick={addPost}>Add post</button>
